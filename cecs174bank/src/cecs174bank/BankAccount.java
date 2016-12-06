628998c1a2b2526e303513af2da7df157806cd6c
CarswellChris279 <carswell_chris279@yahoo.com>
2016-12-06 15:41:38 -0800

    Started writing the BankAccount class

--- cecs174bank/src/cecs174bank/BankAccount.java
+++ cecs174bank/src/cecs174bank/BankAccount.java
@@ -1,6 +1,31 @@
 package cecs174bank;
 
-public class BankAccount {
-	
+import java.util.GregorianCalendar;
+import java.util.Date;
 
+
+public class BankAccount {
+	public String AccountOwner;
+	public enum AccountType {
+		CHECKING("checking"), SAVINGS("savings"), MONEY_MARKET("money market");
+		
+		private String typeOfAccount;
+		private AccountType(String type)
+		{
+			this.typeOfAccount = type;
+		}
+		public String toString()
+		{
+			return this.typeOfAccount;
+		}
+	}
+	
+	public float CurrentBalance;
+	public float InterestRate;
+	public Date DateOpened = new Date();
+	
+	
+	
 }
+
+
